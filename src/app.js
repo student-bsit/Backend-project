13bf94256b4603aedfbc1f7dff1f616eb1f89ea8
student-bsit <ahteshamrauf9@gmail.com>
2025-02-14 09:28:29 -0800

    adding config

--- src/app.js
+++ src/app.js
@@ -2,4 +2,25 @@ const express=require('express');
 
 const app=express();
 
+const cookieParser=require('cookie-parser')
+const cors=require('cors')
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//inbuilt middleware of express
+app.use(express.json({limit:"12kb"}))
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:"12kb"
+}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+
 module.exports=app
