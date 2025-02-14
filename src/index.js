67db490defabaae08f6fc7924672a8ac54d4fedd
student-bsit <ahteshamrauf9@gmail.com>
2025-02-14 08:56:25 -0800

    addition

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
-import dotenv from 'dotenv';
+const dotenv=require('dotenv')
 dotenv.config();
 
+const app=require('./app.js')
 
-dotenv.config({
-    path:"./env"
-})
 
-import connectDB from './db/database.js';
+const connectDB=require( './db/database.js');
 
- connectDB();
+ connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port: ${process.env.PORT}`)
+    })
+ }).catch((err)=>{
+    console.log("Mongodb connection error",errr);
+ })
